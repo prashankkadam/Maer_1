d2ad0341e8798cd1025f6baac31e27624133b364
prashank kadam <prashankkadam07@gmail.com>
2019-09-29 09:57:56 +0530

    Updating the comments
    Updating the comments

--- pdf_static.py
+++ pdf_static.py
@@ -1,10 +1,18 @@
+# importing the required libraries
 import pandas as pd
 from fpdf import FPDF
 
 
+# function for adding an image to the pdf object
 def add_image(image_path, dataframe):
+
+    # Creating a pdf object 
     pdf = FPDF()
+
+    # Adding a page layout to the pdf
     pdf.add_page()
+
+    # Importing the image and Setting the image size
     pdf.image(image_path, x=7.5, y=6, w=75)
     pdf.set_font("Arial", size=12)
     pdf.ln(20)  # move 20 down
@@ -16,18 +24,24 @@ def add_image(image_path, dataframe):
 
     pdf.ln(10)
     dataframe = dataframe.loc[:, 'Vessel':'Type']
-
+    
+    # Converting the dataframe to list
     data = dataframe.values.tolist()
     pdf.set_font("Arial", size=12)
     spacing = 1
+
+    # Setting the row and column dimensions 
     col_width = pdf.w / 4.5
     row_height = 15
+
+    # Adding the data from the dataframe to the pdf table 
     for row in data:
         for item in row:
             pdf.cell(col_width, row_height * spacing,
                      txt=str(item), border=1)
         pdf.ln(row_height * spacing)
-
+ 
+    # Generating the output file 
     pdf.output("add_image.pdf")
 
 
